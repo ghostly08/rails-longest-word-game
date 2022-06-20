1d792f5c1b7cb647185db8c17ef3fd2439b1acdd
ghostly08 <steven.produccionbcn@gmail.com>
2022-06-20 21:44:27 +0200

    a bit styling on titles

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
 
   def new
-    @letters = ('a'..'z').to_a.shuffle[0..9].join(" ")
+    @letters = ('a'..'z').to_a.shuffle[1..9].join(" ")
   end
 
   def score
