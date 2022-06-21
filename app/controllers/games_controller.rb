79d214d53df0ddf554aa331d579bee4f6f379402
ghostly08 <steven.produccionbcn@gmail.com>
2022-06-21 13:35:10 +0200

    changing the form and convert letters from an array to string

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,17 @@
+require "open-uri"
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
 
   def new
-    @letters = ('a'..'z').to_a.shuffle[1..9].join(" ")
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(4) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
+    # @letters.join(", ")
   end
 
   def score
-   @letters = params[:letters].split
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
   end
 end
