123133eb2150ee9e59bb2e90d4834670d8dcb016
ghostly08 <steven.produccionbcn@gmail.com>
2022-06-22 13:32:39 +0200

    connected to english dictionary API

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,11 +13,18 @@ class GamesController < ApplicationController
     @letters = params[:letters].split
     @word = (params[:word] || "").upcase
     @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
   end
 
   private
 
   def included?(word, letters)
     word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
